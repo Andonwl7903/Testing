da3dcb3b591556d9d9ecc44f8b94fd8fefede7b8
Andon Langley <andonwl@gmail.com>
2026-03-16 15:17:53 -0400

    Delete Alter Table_prereq_attrec.sql

--- Alter Table_prereq_attrec.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE prerequisites
-	DROP PRIMARY KEY,
-	ADD COLUMN id INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST,
-    ADD UNIQUE KEY uq_prereq (course_id, prereq_course_id);
-    
-ALTER TABLE attendance_records
-	DROP PRIMARY KEY,
-	ADD COLUMN id INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST,
-    ADD UNIQUE KEY uq_att_record (attendance_session_id, student_id);
